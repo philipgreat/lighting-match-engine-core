b11db07cd39963444b97e74f47f1a7a36afb5b99
Philip Zhang <zhangxilai@doublechaintech.com>
2025-10-13 10:03:02 +0800

    🚀fix name

--- menu.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO config_item_data (id, config_type, value, remark, create_time, merchant, version) VALUES (2, 2, '欢迎你反馈你的良好建议, 共创发展环境。<br>反馈任何不良违规行为, 工作环境更干净。<br><b>绝对保密</b>, 所有数据均加密保存,<b>查实有奖！</b>', '反馈首页文字', '2025-10-10 12:03:38', 1, 1);
-
-
